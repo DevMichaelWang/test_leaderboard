ae493fe2af2c9528c5870141224b414c755e8798
Michael Wang <dev.michaelwang@gmail.com>
2023-05-23 18:07:11 +0800

    remove comments

--- src/store/UserState.tsx
+++ src/store/UserState.tsx
@@ -1,27 +1,3 @@
-// import { useState } from 'react';
-
-// interface ContextType {
-//     username: string;
-//     setUserName: (username: string) => void;
-// }
-  
-
-
-// const UserState = () => {
-
-//     const [username, setUser] = useState('');
-//     const setUserName = (name: string) => {        
-//         setUser(name);
-//     };
-
-//     return {
-//         username,
-//         setUserName,
-//     };
-// };
-
-// export default UserState;
-
 import { useState } from 'react';
 import {User} from '../interfaces/User'
 
